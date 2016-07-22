5e3a80468d9114b25dd50ec64fe8f5e33a3c061c
Progen <progen.japamony@barclays.com>
2016-07-22 11:38:14 +0800

    Added a cache for bitmaps

---
+++ app/src/main/java/com/movies/bten/utils/cache/LruBitmapCache.java
@@ -0,0 +1,44 @@
+package com.movies.bten.utils.cache;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+import android.util.DisplayMetrics;
+
+import com.android.volley.toolbox.ImageLoader;
+
+/**
+ * Created by Progen on 22/7/16.
+ */
+public class LruBitmapCache extends LruCache<String, Bitmap>
+        implements ImageLoader.ImageCache {
+
+
+    public LruBitmapCache(int maxSize) {
+        super(maxSize);
+    }
+
+    public LruBitmapCache(Context ctx) {
+        this(getCacheSize(ctx));
+    }
+
+    @Override
+    public Bitmap getBitmap(String url) {
+        return null;
+    }
+
+    @Override
+    public void putBitmap(String url, Bitmap bitmap) {
+
+    }
+
+    public static int getCacheSize(Context ctx) {
+        final DisplayMetrics displayMetrics = ctx.getResources().
+                getDisplayMetrics();
+        final int screenWidth = displayMetrics.widthPixels;
+        final int screenHeight = displayMetrics.heightPixels;
+        // 4 bytes per pixel
+        final int screenBytes = screenWidth * screenHeight * 4;
+        return screenBytes * 3;
+    }
+}
