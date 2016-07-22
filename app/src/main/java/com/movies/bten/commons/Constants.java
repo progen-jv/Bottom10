a49c91069f36143e71775a87a6186d095372ee3f
Progen <progen.japamony@barclays.com>
2016-07-22 08:55:39 +0800

    The api's to be used.

---
+++ app/src/main/java/com/movies/bten/commons/Constants.java
@@ -0,0 +1,19 @@
+package com.movies.bten.commons;
+
+/**
+ * Created by Progen on 22/7/16.
+ */
+public class Constants {
+    //- Search
+    //https://api.themoviedb.org/3/discover/movie?primary_release_year=2014&with_genres=28&sort_by=vote_count.asc&include_video=true&api_key=3286a767ba08915afee23cefadaa4f1a
+    //- Genre List
+    //http://api.themoviedb.org/3/genre/movie/list?api_key=3286a767ba08915afee23cefadaa4f1a
+    // - Image repo
+    //https://image.tmdb.org/t/p/w370/qfl2VEQzbXOUWIlXGfUbFH0rMtZ.jpg
+    // - Trailers
+    //http://api.themoviedb.org/3/movie/296092/videos?api_key=3286a767ba08915afee23cefadaa4f1a
+    // - Movie details
+    //http://api.themoviedb.org/3/movie/253649?api_key=3286a767ba08915afee23cefadaa4f1a
+    // - Credits
+    //http://api.themoviedb.org/3/movie/118340/credits?api_key=3286a767ba08915afee23cefadaa4f1a
+}
