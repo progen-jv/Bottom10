6ff76a7c10676a6e4988770d6748d6fbeb550334
Progen <progen.japamony@barclays.com>
2016-07-22 15:42:06 +0800

    Added actionbar to main screen

--- app/src/main/java/com/movies/bten/MainActivity.java
+++ app/src/main/java/com/movies/bten/MainActivity.java
@@ -1,18 +1,62 @@
 package com.movies.bten;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolBarMain);
+        setSupportActionBar(myToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem yearItem = menu.findItem(R.id.spinner_year);
+        Spinner yearSpinner = (Spinner) MenuItemCompat.getActionView(yearItem);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.years_array, android.R.layout.simple_spinner_dropdown_item);
+        yearSpinner.setAdapter(adapter);
+        yearSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        MenuItem genreItem = menu.findItem(R.id.spinner_genre);
+        Spinner genreSpinner = (Spinner) MenuItemCompat.getActionView(genreItem);
+        ArrayAdapter<CharSequence> genreAdapter = ArrayAdapter.createFromResource(this, R.array.genre_arry, android.R.layout.simple_spinner_dropdown_item);
+        genreSpinner.setAdapter(genreAdapter);
+        genreSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
