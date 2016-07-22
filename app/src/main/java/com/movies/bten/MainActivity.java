ed2ac29c93e0bc6579dfc50b5f8b2f1de3e53172
Progen <progen.japamony@barclays.com>
2016-07-22 09:00:39 +0800

    Added volley library for easy http

--- app/src/main/java/com/movies/bten/MainActivity.java
+++ app/src/main/java/com/movies/bten/MainActivity.java
@@ -3,11 +3,16 @@ package com.movies.bten;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
     }
 }
